1be69037a712786336a97f6f34231f9923c4ee32
Bowan Gerry <Bowan.Gerry@Focusservices.com>
2017-10-19 19:37:44 -0600

    test

--- lib/kuma_server_dev_web/controllers/page_controller.ex
+++ lib/kuma_server_dev_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule KumaServerDevWeb.PageController do
   use KumaServerDevWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    json conn, %{"kuma" => true}
   end
 end
