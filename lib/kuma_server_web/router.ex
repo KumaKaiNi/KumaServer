abe4c475ab7735e4c9ca100914283bee5e8ce398
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-19 20:13:22 -0600

    plug test

--- lib/kuma_server_web/router.ex
+++ lib/kuma_server_web/router.ex
@@ -3,11 +3,17 @@ defmodule KumaServerWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :auth
   end
 
   scope "/", KumaServerWeb do
     pipe_through :api # Use the default browser stack
 
     post "/", ApiController, :handle
+  end
+
+  def auth(conn, _opts) do
+    IO.inspect conn.req_headers
+    conn
   end
 end
