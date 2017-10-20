0703e5d45d0a890cef39060eda76a02de5194d13
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-19 20:58:35 -0600

    atomize map

--- lib/kuma_server_web/controllers/api_controller.ex
+++ lib/kuma_server_web/controllers/api_controller.ex
@@ -4,12 +4,21 @@ defmodule KumaServerWeb.ApiController do
 
   # curl -XPOST -H 'Content-Type: application/json' -H 'Auth: test' --data-binary '{"content": {"message":{"text":"!ping"}}}' dev.riichi.me
   def handle(conn, params) do
-    data = for {key, val} <- params, into: %{}, do: {String.to_atom(key), val}
+    data = atomize_map(params)    
     json conn, parse(data.content)
   end
 
   defp parse(data) do
     match "!ping", do: %{text: "Pong!"}
     match "!foo", do: %{text: "Bar!"}
+  end
+
+  defp atomize_map(struct) do
+    struct |> Enum.reduce %{}, fn({key, val}, acc) ->
+      cond do
+        is_map(val) -> Map.put(acc, String.to_atom(key), atomize_map(val))
+        true        -> Map.put(acc, String.to_atom(key), val)
+      end
+    end
   end
 end
