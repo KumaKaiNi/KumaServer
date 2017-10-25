db4b20f9da22d33308af297190176187911a762e
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-25 13:55:58 -0600

    ohisee

--- lib/kuma_server/logger.ex
+++ lib/kuma_server/logger.ex
@@ -13,35 +13,35 @@ defmodule KumaServer.Logger do
 
   def to_console(:recv, content) do
     cond do
-      content.source.channel.private ->
-        Logger.info "[#{content.source.protocol}] #{if content.source.guild.name, do: "[" <> content.source.guild.name <> "] "}[private] #{content.user.name}: #{content.message.text}"
+      content.channel.private ->
+        Logger.info "[#{content.protocol}] #{if content.guild.name, do: "[" <> content.guild.name <> "] "}[private] #{content.user.name}: #{content.message.text}"
       true ->
-        Logger.info "[#{content.source.protocol}] [#{content.source.guild.name}] [#{if content.source.channel.nsfw, do: '!'}\##{content.source.channel.name}] #{if content.user.moderator, do: '+'}#{content.user.name}: #{content.message.text}"
+        Logger.info "[#{content.protocol}] [#{content.guild.name}] [#{if content.channel.nsfw, do: '!'}\##{content.channel.name}] #{if content.user.moderator, do: '+'}#{content.user.name}: #{content.message.text}"
     end
   end
 
   def to_console(:send, content, message) do
     cond do
-      content.source.channel.private ->
-        Logger.info "[#{content.source.protocol}] #{if content.source.guild.name, do: "[" <> content.source.guild.name <> "] "}[private] kumakaini: #{message}"
+      content.channel.private ->
+        Logger.info "[#{content.protocol}] #{if content.guild.name, do: "[" <> content.guild.name <> "] "}[private] kumakaini: #{message}"
       true ->
-        Logger.info "[#{content.source.protocol}] [#{content.source.guild.name}] [#{if content.source.channel.nsfw, do: '!'}\##{content.source.channel.name}] +kumakaini: #{message}"
+        Logger.info "[#{content.protocol}] [#{content.guild.name}] [#{if content.channel.nsfw, do: '!'}\##{content.channel.name}] +kumakaini: #{message}"
     end
   end
 
   def to_file(:recv, content) do
     logfolder = cond do
-      content.source.channel.private ->
-        "/home/bowan/bots/_log/#{content.source.protocol}#{if content.source.guild.name, do: "/" <> content.source.guild.name}/private"
+      content.channel.private ->
+        "/home/bowan/bots/_log/#{content.protocol}#{if content.guild.name, do: "/" <> content.guild.name}/private"
       true ->
-        "/home/bowan/bots/_log/#{content.source.protocol}/#{content.source.guild.name}"
+        "/home/bowan/bots/_log/#{content.protocol}/#{content.guild.name}"
     end
 
     unless File.exists?(logfolder), do: File.mkdir_p(logfolder)
 
     logfile = cond do
-      content.source.channel.private -> "#{content.user.name}.log"
-      true -> "#{content.source.channel.name}.log"
+      content.channel.private -> "#{content.user.name}.log"
+      true -> "#{content.channel.name}.log"
     end
 
     time = DateTime.utc_now |> DateTime.to_iso8601
@@ -52,21 +52,21 @@ defmodule KumaServer.Logger do
 
   def to_file(:send, content, message) do
     logfolder = cond do
-      content.source.channel.private ->
-        "/home/bowan/bots/_log/#{content.source.protocol}#{if content.source.guild.name, do: "/" <> content.source.guild.name}/private"
+      content.channel.private ->
+        "/home/bowan/bots/_log/#{content.protocol}#{if content.guild.name, do: "/" <> content.guild.name}/private"
       true ->
-        "/home/bowan/bots/_log/#{content.source.protocol}/#{content.source.guild.name}"
+        "/home/bowan/bots/_log/#{content.protocol}/#{content.guild.name}"
     end
 
     unless File.exists?(logfolder), do: File.mkdir(logfolder)
 
     logfile = cond do
-      content.source.channel.private -> "#{content.user.name}.log"
-      true -> "#{content.source.channel.name}.log"
+      content.channel.private -> "#{content.user.name}.log"
+      true -> "#{content.channel.name}.log"
     end
 
     kuma = cond do
-      content.source.channel.private -> "kumakaini"
+      content.channel.private -> "kumakaini"
       true -> "+kumakaini"
     end
 
